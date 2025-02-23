b9a4b5b4ad01d3e0b446571b230970d5a3eae154
Bintang Naufal <1bintangnaufal@gmail.com>
2025-02-23 20:48:51 +0700

    chore: grid

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <>
       <StoreHeader />
-      <div className="grid grid-cols-2 grid-rows-1 gap-2">
+      <div className="grid grid-cols-4 grid-rows-1 gap-2">
         <FilterIsland className="flex items-center justify-center h-dvh border border-red-500" />
-        <main className="flex items-center justify-center h-dvh border border-red-500">
+        <main className="col-span-3 flex items-center justify-center h-dvh border border-red-500">
           Main content of this page goes here
         </main>
       </div>
